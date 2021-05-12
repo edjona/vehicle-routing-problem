1f7da13396e0952855de1634cd15098b35d6ee04
Jonathan Edward <jonathan.edward.1112049@gmail.com>
2021-05-13 02:32:30 +0700

    Create main class

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import config.CustomLogger;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger logger = CustomLogger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+
+    }
+}
