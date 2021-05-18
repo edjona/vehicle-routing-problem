7a246dbfaa19be92cfa5759655c7de38a82ee3ee
palpaul18 <paulpaulus18@gmail.com>
2021-05-18 12:35:17 +0700

    Selection

---
+++ src/main/java/process/CrossOver.java
@@ -0,0 +1,16 @@
+package process;
+
+import model.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CrossOver {
+    public static void swapKromosom (int indexParent1, int indexParent2){
+        List<Customer> partialMapXOver = new ArrayList<>(){
+
+        };
+        return;
+    }
+
+}
---
+++ src/main/java/process/Selection.java
@@ -0,0 +1,17 @@
+package process;
+
+
+import model.Customer;
+import model.Demand;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Selection {
+    public static List<Customer> selectedParent(int indexParent1, int indexParent2) {
+        List<Customer> roulleteWheel = new ArrayList<>();
+
+
+        return roulleteWheel;
+    }
+}
